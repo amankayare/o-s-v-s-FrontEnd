35547fcc4308e5e309fb591ff378ac5aee0ad43b
Pravin Jeurkar <jeurkarpravin96@gmail.com>
2021-01-20 22:20:58 +0530

    conflict resolved

--- src/app/Entities/candidate.ts
+++ src/app/Entities/candidate.ts
@@ -2,7 +2,7 @@ export class Candidate {
     public candidateId : number | undefined;
     public fullName : string | undefined;
     public email : string | undefined;
-    public symbol : string | undefined;
+    public symbol : File | undefined;
     public voteEarned : number | undefined;
     public electionId : number | undefined;
     public candidateElectionList : Candidate[] | undefined;
---
+++ src/app/Entities/organization.ts
@@ -0,0 +1,6 @@
+export class Organization {
+    private id : number | undefined;
+    private orgnizationName : String | undefined;
+    private excelFile : String | undefined;
+    private cin : String | undefined;
+}
