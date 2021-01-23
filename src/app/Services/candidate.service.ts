e405741a4014eea33447fb442aaf25b6826ed111
Pravin Jeurkar <jeurkarpravin96@gmail.com>
2021-01-23 15:47:24 +0530

    creating functionality of eBallot

--- src/app/Services/candidate.service.ts
+++ src/app/Services/candidate.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Candidate } from '../Entities/candidate';
+import { Election } from '../Entities/election';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidateService {
 
+ // election: Election =new Election();
+  
   constructor(private http:HttpClient) { }
 
   
--- src/app/createballot/createballot.component.ts
+++ src/app/createballot/createballot.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Candidate } from '../Entities/candidate';
 import { Election } from '../Entities/election';
+import { CandidateService } from '../Services/candidate.service';
 import { ElectionService } from '../Services/election.service';
 
 declare var $: any;
@@ -14,18 +15,28 @@ declare var jQuery: any;
 })
 export class CreateballotComponent implements OnInit {
 
-  Election: Election =new Election();
+  Election: Election =new Election(); ///for binding
 
   
 
-  constructor(private electionService:ElectionService,private router:Router,) { }
+  constructor(private electionService:ElectionService,private router:Router,private candidateService:CandidateService) { }
 
   ngOnInit()  {
-    this.electionService.election;
+   
     }
 
     addElection(){
      // this.router.navigate(['/reserve',JSON.stringify(this.Election)]‌​);
+      this.electionService.election.cin=this.Election.cin;
+      this.electionService.election.startDate=this.Election.startDate;
+      this.electionService.election.endDate=this.Election.endDate;
+      this.electionService.election.resultDate=this.Election.resultDate;
+      this.electionService.election.electionName=this.Election.electionName;
+      this.electionService.election.voterList=[];
+      this.electionService.election.candidateList=[];
+      console.log(this.Election);
+      console.log(this.electionService.election);
+      this.router.navigate(['E-Ballot/api/candidatelist']);
     }
 
   }
